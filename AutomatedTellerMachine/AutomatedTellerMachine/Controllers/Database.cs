e57bcd90400659ad1ac171672c3d6724e60cbb6d
jims6602 <112780571+jims6602@users.noreply.github.com>
2022-09-03 23:10:03 -0400

    Add files via upload

---
+++ AutomatedTellerMachine/AutomatedTellerMachine/Controllers/Database.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedTellerMachine.Controllers
+{
+    internal class Database
+    {
+        public string connectionString { get; set; }
+
+        public Database()
+        {
+            /*
+            connectionString = "Data Source=DESKTOP-CV57V7N\\SQLEXPRESS;Initial Catalog=SummerTimeBank;"
+            + "Integrated Security=True";
+            */
+
+            connectionString = "Data Source=DESKTOP-T4TERMR\\SQLEXPRESS;Initial Catalog=SummerTimeBank;"
+            + "Integrated Security=True";
+
+        }
+    }
+}
---
+++ AutomatedTellerMachine/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using AutomatedTellerMachine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: /Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //GET: /Home/About
+        public ActionResult About()
+        {
+            ViewBag.Message = "The Countries which Summer Time Bank operates in.";
+
+            ViewBag.Countries = new List<String>()
+            {
+                "India",
+                "United States",
+                "Canada",
+                "Kenya"
+
+            };
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.CustomerMessage = "Having trouble? Send us a message.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string customerMessage)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+
+            ViewBag.CustomerMessage = "Thanks, we got your message !";
+
+            var database = new Database();
+            
+            cmd.CommandText = "INSERT INTO  dbo.comments (message) VALUES ('"+ customerMessage + "')";
+
+            using (SqlConnection conn =
+            new SqlConnection(database.connectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+
+                try
+                {
+                    
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        
+          return View();
+        }
+
+        //GET: /Home/Serial/lower
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "STB 18.01.30.05";
+
+            if( letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+
+            return Content(serial);
+        }
+    }
+}
